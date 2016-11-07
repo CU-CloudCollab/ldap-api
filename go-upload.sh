148d00c99d6a2e56d449795c2fd43751623ae708
Paul Allen <pea1@cornell.edu>
2016-11-07 12:39:09 -0500

    fix depencies

--- go-upload.sh
+++ go-upload.sh
@@ -6,8 +6,7 @@
 source ./constants.sh
 
 # zip contents
-zip -qr lambda-code.zip lambda.js node_modules/moment-timezone node_modules/moment
-# zip -qr lambda-code.zip lambda.js node_modules/node-json2html node_modules/node-rest-client
+zip -qr lambda-code.zip lambda.js node_modules/*
 
 # load to S3
 aws s3 cp ./lambda-code.zip s3://$S3BUCKET/$CODE_ZIPFILE
